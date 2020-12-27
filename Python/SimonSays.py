f95ee698b97f157df786556588448b9ca9b08406
tomasitobambito <tomgacek0604@gmail.com>
2020-12-27 19:34:15 +0100

    added comments

--- Python/SimonSays.py
+++ Python/SimonSays.py
@@ -1,7 +1,7 @@
 from microbit import *
 import random
 
-class Light:
+class Light: #custom light commands for easier understanding and automation with arrays
     def on(colour):
         if colour == 1:
             pin13.write_digital(1)
@@ -18,22 +18,22 @@ class Light:
         else:
             pin15.write_digital(0)
 
-class Game:
+class Game: #contains all variables and functions for the main game part
     combination = []
     playerInput = []
     integer = 0
 
-    def __init__(self, length):
+    def __init__(self, length): #creates initial combination
         i = 0
         while i < length:
             integer = random.randint(1, 3)
             if len(self.combination) >= 2:
-                if self.combination[i - 2] == integer and self.combination[i - 1] == integer:
+                if self.combination[i - 2] == integer and self.combination[i - 1] == integer: #checks if a number is repeating more than twice
                     continue
             self.combination.append(integer)
             i += 1
     
-    def hardReset(self, length):
+    def hardReset(self, length): #used on failed attempt to reset variables and create new combination
         self.combination = []
         self.playerInput = []
         i = 0
@@ -45,16 +45,14 @@ class Game:
             self.combination.append(integer)
             i += 1
     
-    def inputReset(self):
-        self.playerInput = []
-    
-    def increment(self):
+    def increment(self): #increments combination by 1
         while True:
             integer = random.randint(1, 3)
-            if self.combination[-2] == integer and self.combination[-1] == integer:
+            if self.combination[-2] == integer and self.combination[-1] == integer: #checks if a number is repeating more than twice
                 continue
             self.combination.append(integer)
             break
+        self.playerInput = [] #resets input
     
     def verifyInput(self, index):
         if self.combination[index] != self.playerInput[index]:
@@ -65,7 +63,7 @@ gameObj = Game(3)
 success = True
 
 while True:
-    for n in range(7):
+    for n in range(7): #eventual combination length of 9
 
         #display combination
         for j in gameObj.combination:
@@ -74,8 +72,7 @@ while True:
             Light.off(j)
             sleep(500)
 
-        #defines and resets counter
-        i = 0
+        i = 0 #defines and resets counter
 
         #user tries to guess combination
         while i < (n + 3):
@@ -101,20 +98,21 @@ while True:
                 i += 1
                 sleep(500)
         
+        #restarts game on unsuccessful try 
         if not success:
             Light.on(1)
-            sleep(2000)
+            sleep(4000)
             Light.off(1)
-            sleep(500)
+            sleep(2000)
             gameObj.hardReset(3)
             break
 
         #increment to next combination
         gameObj.increment()
-        gameObj.inputReset()
 
+    #win condition; ends game
     if success:
         Light.on(3)
         break
 
-    success = True
+    success = True #resets success variable after failed attempt
